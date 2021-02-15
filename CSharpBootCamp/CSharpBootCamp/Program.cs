54bebdb535c28512345e7f8a5ddb384d5cd555a5
youssef reddam <youssef.reddam.98@hotmail.com>
2021-02-15 16:19:27 +0100

    Added new branch for Fonctions lesson homework.

--- CSharpBootCamp/CSharpBootCamp/Program.cs
+++ CSharpBootCamp/CSharpBootCamp/Program.cs
@@ -6,7 +6,49 @@ namespace CSharpBootCamp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HomeworkDemo();
+        }
+
+        private static void HomeworkDemo()
+        {
+            string name = GetString("Enter your name: ");
+            string surename = GetString("Enter your surename: ");
+
+            int age = GetInt("Enter your age: ");
+
+            float weight = GetFloat("Enter your weight: ");
+            float height = GetFloat("Enter your height: ");
+
+            Console.WriteLine($"{name} {surename} is {age} years old, " +
+                $"his weight is {weight} kg and his height is {height} cm.");
+
+            ShowBMI(weight, height);
+        }
+
+        private static string GetString(string msg)
+        {
+            Console.WriteLine(msg);
+            string name = Console.ReadLine();
+            return name;
+        }
+        private static int GetInt(string msg)
+        {
+            Console.WriteLine(msg);
+            int name = int.Parse(Console.ReadLine());
+            return name;
+        }
+        private static float GetFloat(string msg)
+        {
+            Console.WriteLine(msg);
+            float name = float.Parse(Console.ReadLine());
+            return name;
+        }
+
+        private static void ShowBMI(float w, float h)
+        {
+            h = h / 100;
+            float BMI = w / h / h;
+            Console.WriteLine($"Your body mass index(BMI) is: {BMI}.");
         }
     }
 }
